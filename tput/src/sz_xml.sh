6572a02256c995a3e89e1d0deec31051c866220b
zhenxun815 <guoyiheng89@gmail.com>
2019-04-19 12:03:10 +0800

    删除路径下所有子文件夹中.ok与.rst文件

--- tput/src/sz_xml.sh
+++ tput/src/sz_xml.sh
@@ -3,18 +3,31 @@
 #description: 
 #create: 4/18/2019
 
+function judgeDirExists() {
+    dir=$1
+    if [[ ! -d "$dir" ]];then
+    mkdir ${dir}
+    fi
+}
+
 find $(pwd)/CN_*  -maxdepth 2 -mindepth 2 -type d > find.txt
 echo "find xml dir complete..."
+
+zips_dir=$(pwd)/zips/
+judgeDirExists ${zips_dir}
+echo "zips dir is: $zips_dir"
 
 for line in $(cat ./find.txt) ; do
     echo "file to zip is $line"
     source_dir_path=${line%/*}
     source_dir_name=${line##*/}
 
-    zip_file_dir=${source_dir_path##*/}
-    zip_file_name=${source_dir_name}
+    zip_file_dir_name=${source_dir_path##*/}
+    zip_file_dir=$(pwd)/zips/${zip_file_dir_name}
+    judgeDirExists ${zip_file_dir}
 
-    zip_file_path=$(pwd)/zips/${zip_file_dir}/${zip_file_name}.zip
+    zip_file_name=${source_dir_name}
+    zip_file_path=${zip_file_dir}/${zip_file_name}.zip
     echo "zip file path is: $zip_file_path"
     zip -r ${zip_file_path} ${line}
 done
@@ -28,5 +41,3 @@ for line in $(cat ./zip.txt) ; do
     #sz $line
 done
 echo "sz complete..."
-
-
